d18fcef9cc4249a920a94160315789f3629c5c7b
balu2750 <balu2750@gmail.com>
2018-05-03 21:02:50 +0530

    hello world

---
+++ hello.java
@@ -0,0 +1,17 @@
+hello.java
+
+public class hello{
+
+      public static void main(string[] args){
+   
+          for(int i=1;i<=10;i+=){
+
+   system.out.println("hello world ..."+i);
+
+        }
+
+    }
+
+
+ }
+
